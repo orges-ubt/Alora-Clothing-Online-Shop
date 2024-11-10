fa536a36ab3b21170ac45208e2b695d9cb5d9385
orges-ubt <os70476@ubt-uni.net>
2024-11-10 23:32:28 +0100

    Log In Form Validation

--- script.js
+++ script.js
@@ -28,3 +28,26 @@ function nextSlide() {
     slideIndex = (slideIndex + 1) % slides.length;
     showSlide(slideIndex);
 }
+
+//Log In Form Validation
+function validateLoginForm() {
+    const username=document.getElementById("loginUsername").value;
+    const password=document.getElementById("loginPassword").value;
+    const errorMessage=document.getElementById("errorMessage");
+
+    errorMessage.textContent="";
+
+if(!username) {
+    alert("Please enter your correct username!");
+    return false;
+}
+if (!password) {
+    alert("Please enter your correct password!");
+}
+    const passwordPattern= /^(?=.*[0-9])(?=.*[A-Z])[A-Za-z\d]{8,}$/;
+if (!passwordPattern.test(password)) {
+    errorMessage.textContent = "Password must be at least 8 characters, start with an uppercase letter, and contain at least one number.";
+    return false;
+}
+    return true;    
+}
