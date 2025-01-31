c65fe62c743a43973afff9a570a6d6cc23b6f5d1
algas-ubt <ag70166@ubt-uni.net>
2025-01-31 21:33:25 +0100

    Database Connection

---
+++ database.php
@@ -0,0 +1,19 @@
+<?php
+class database {
+    private $host = "localhost"; 
+    private $user = "root"; 
+    private $pass = ""; 
+    private $dbname = "Alora"; 
+    private $conn;
+    public function __construct() {
+        $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->dbname);
+        
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+    public function getConnection() {
+        return $this->conn;
+    }
+}
+?>
