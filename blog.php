0da73a3ef2811228b900752524a8f4bac2cdd230
algas-ubt <ag70166@ubt-uni.net>
2025-02-01 22:55:09 +0100

    Implementing Navigation Bar at Blog Page

--- blog.php
+++ blog.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 require_once "post.php";
 
 $postObj = new Post();
@@ -67,6 +66,7 @@ $posts = $postObj->getPosts();
 <html>
 <head>
     <title>Blog Reviews</title>
+    <link rel="stylesheet" href="style.css">
     <style>
         * {
             box-sizing: border-box;
@@ -166,6 +166,35 @@ $posts = $postObj->getPosts();
     </style>
 </head>
 <body>
+<nav class="navbar">
+  <div id="nav-logo">
+    <a href="index.php"><img src="images/logo.png" alt="Alora Logo"></a>
+  </div>
+  <div class="nav-ul">
+    <ul>
+      <?php
+      session_start();
+      if (isset($_SESSION['user_id']) && isset($_SESSION['role']) && $_SESSION['role'] === 'admin') {
+          echo '<li id="dashboard"><a href="dashboard.php">Dashboard</a></li>';
+      }
+      ?>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="products.php">Products</a></li>
+      <li><a href="about.php">About Us</a></li>
+      <li><a href="blog.php">Blog</a></li>
+      <?php
+      if (!isset($_SESSION['user_id'])) {
+          echo '<li><a href="login.php"><svg width="33px" height="33px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="black" class="user"><path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" /></svg></a></li>';
+      }
+      ?>
+      <?php
+      if (isset($_SESSION['user_id'])) {
+          echo '<li><a href="logout.php" style="padding: 10px 15px; background-color: rgb(212, 203, 203);color:white; text-decoration: none; border-radius: 4px;">Logout</a></li>';
+      }
+      ?>
+    </ul>
+  </div>
+</nav>
 <div class="container">
     <h1>Blog Reviews</h1>
     <?php if (isset($_SESSION['user_id'])) { ?>
